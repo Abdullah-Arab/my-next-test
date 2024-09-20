7c014f82e9149f805c4b92417f00aba0cd5b83de
Abdullah Arab <abdo.arab2014@gmail.com>
2024-09-20 02:08:07 +0200

    Refactor: Update import path for ThemeProvider in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import {
   Projects,
   Skills,
 } from "@/app/components/portfolio";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -37,24 +38,28 @@ export default function RootLayout({}: Readonly<{
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <main>
-          <Home />
-          <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-              <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-                Turning ideas into apps that matter
-              </p>
-            </div>
-          </BackgroundGradientAnimation>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="min-h-screen bg-white text-neutral-950 dark:bg-neutral-950 dark:text-neutral-50">
+            <Navbar />
+            <main>
+              <Home />
+              <BackgroundGradientAnimation>
+                <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+                  <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+                    Turning ideas into apps that matter
+                  </p>
+                </div>
+              </BackgroundGradientAnimation>
 
-          <About />
-          <Skills />
-          <Projects />
-          {/* <Experience /> */}
-          <Contact />
-        </main>
-        <Footer />
+              <About />
+              <Skills />
+              <Projects />
+              {/* <Experience /> */}
+              <Contact />
+            </main>
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
